18daa32329e08aad04002277285c882fa1ca9bfd
frver0x <nicecapybara@proton.me>
2025-12-12 18:40:54 +0100

    Mes modifications

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ def ma_second_fonction(inuput_string2: str) -> str:
     return inuput_string2[::-1]
 
 print("Hello from test.py")
+
+ekdrnjograhlgr
+rg
+rag,kglronhragnograùr
+grrgkngrojigrmagr
+grngrngrùg
+gra
+gr
+agr
+raerg,kllllzo
